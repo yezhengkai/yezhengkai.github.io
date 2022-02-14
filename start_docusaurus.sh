bb17f3b55e6ec056beefd1b96b8669479d569344
kai <supon3060@gmail.com>
2022-02-14 14:34:53 +0000

    Use "watch" instead of "poll"

--- start_docusaurus.sh
+++ start_docusaurus.sh
@@ -5,4 +5,6 @@ set -u
 set -o pipefail
 
 cd website
-yarn start --poll 1000
+yarn start
+# Use polling of files rather than watching for live reload as a fallback in environments where watching doesn't work.
+#yarn start --poll 1000
