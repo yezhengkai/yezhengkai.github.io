1393d9f41ff069cb84854ed2b61b71d7cebc6810
kai <supon3060@gmail.com>
2022-03-05 14:47:24 +0000

    Modify home page

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/data-science/julia-infra-and-tool">
+            Go to Notes 📄
           </Link>
         </div>
       </div>
