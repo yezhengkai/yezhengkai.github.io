43085d6d091f9ccaf072bb14b379b6322e595e48
kai <supon3060@gmail.com>
2022-05-02 10:44:27 +0000

    Support for i18n and `yarn start` input arguments

--- start_docusaurus.sh
+++ start_docusaurus.sh
@@ -5,7 +5,12 @@ set -u
 set -o pipefail
 
 cd website
-yarn start
-# yarn start --locale zh-Hant  # use zh-Hant locale
+
+# Translate plugin data
+# refer to https://docusaurus.io/docs/i18n/tutorial#translate-plugin-data
+yarn run write-translations --locale zh-TW
+
+yarn start $@
+# yarn start --locale zh-TW  # use zh-TW locale
 # Use polling of files rather than watching for live reload as a fallback in environments where watching doesn't work.
 #yarn start --poll 1000
